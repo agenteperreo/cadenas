6ae22027d6d03e3ce64bceae21ca1beb52b64838
igarcia <igarcar@iesnervion.es>
2023-03-01 14:31:36 +0100

    01/03/2023

--- Cadenas/src/ejercicios2/Ejercicio1.java
+++ Cadenas/src/ejercicios2/Ejercicio1.java
@@ -7,10 +7,35 @@ public class Ejercicio1 {
 	public static void main(String[] args) {
 		
 		//Declaración de variables
-		String cadena, cadenaCompilada;
+		String cadena;
+		boolean empieza, termina;
+		
+		//Array que contendra los comentarios
+		String[] comentarios;
 		
 		//Abrimos el escaner
 		Scanner sc = new Scanner(System.in);
+		
+		//Pedimos la cadena al usuario
+		System.out.println("Introduce la cadena: ");
+		cadena=sc.nextLine();
+		
+		//Dividimos la cadena por las barras /
+		comentarios=cadena.split("/");
+		
+		//System.out.println(Arrays.toString(comentarios));
+		
+		for(int i=0; i < comentarios.length; i++) {
+			empieza=comentarios[i].startsWith("*");
+			termina=comentarios[i].endsWith("*");
+			
+			if(!empieza || !termina) {
+				System.out.print(comentarios[i]);
+			} 
+		}
+		
+		//Cerramos el escaner
+		sc.close();
 
 	}
 
---
+++ Cadenas/src/ejercicios2/Ejercicio2.java
@@ -0,0 +1,12 @@
+package ejercicios2;
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		
+		//Declaramos las variables
+		
+
+	}
+
+}
