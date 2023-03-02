8127a41524f9a36ca47e5348b008c033766b58a7
igarcia <igarcar@iesnervion.es>
2023-03-02 12:27:47 +0100

    02/03/2023

--- Cadenas/src/ejercicios/Ejercicio7.java
+++ Cadenas/src/ejercicios/Ejercicio7.java
@@ -1,13 +1,33 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio7 {
 
 	public static void main(String[] args) {
-		
-		//Declaración de variables
-		String cadena;
-		
-		//Abrimos la cadena
+
+		// Introducimos las variables
+		String frase;
+
+		// Creamos el scanner
+		Scanner sc = new Scanner(System.in);
+
+		// Le pedimos al usuario una frase
+		System.out.println("Dime una frase: ");
+		frase = sc.nextLine();
+
+		// Invertimos la frase al revés
+		String fraseInvertida = new StringBuilder(frase).reverse().toString();
+
+		// Comprobamos si es una frase palíndroma
+		if (frase.equals(fraseInvertida)) {
+			System.out.println("La frase es palíndroma");
+		} else {
+			System.out.println("La frase no es palíndroma");
+		}
+
+		// Cerramos el scanner
+		sc.close();
 
 	}
 
